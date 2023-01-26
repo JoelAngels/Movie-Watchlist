517eb2bfc86eb28978fcb8bd9b23aeccf183fac8
Joel Angel <joelbaraka4325@gmail.com>
2023-01-26 09:37:57 +0300

    styled initiated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddMovie from "./features/addMovie";
 import WatchListMovies from "./features/watchListMovies";
 import WatchedMovies from "./features/watchedMovies";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<WatchListMovies />} />
         <Route path="/watched" element={<WatchedMovies />} />
