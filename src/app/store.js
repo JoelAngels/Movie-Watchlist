257b187dc8d2ace4336b7aa7b1dfd23b621e51b3
Joel Angel <joelbaraka4325@gmail.com>
2023-01-26 15:00:34 +0300

    added redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { movieApi } from "../services/movieApi";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
 
 import moviesReducer from "../features/addMovie/moviesSlice";
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  [movieApi.reducerPath]: movieApi.reducer,
+  movies: moviesReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const store = configureStore({
-  reducer: {
-    [movieApi.reducerPath]: movieApi.reducer,
-    movies: moviesReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(movieApi.middleware),
+    getDefaultMiddleware({ serializableCheck: false }).concat(
+      movieApi.middleware
+    ),
 });
